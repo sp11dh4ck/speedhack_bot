81fb281d290daf18a406f404c5e7b8124e0138f3
Zakhar <94287281+sp11dh4ck@users.noreply.github.com>
2022-05-27 17:40:30 +0300

    Update markups.py

--- markups.py
+++ markups.py
@@ -1,6 +1,4 @@
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 # start command
